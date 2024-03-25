e71555b64900851dca9e06c7e19dcd654113aad8
unknown <danielfrancis442@gmail.com>
2024-03-25 09:53:15 +0100

    Learning frontend using JS

---
+++ Frontend_Web_Development_Bootcamp_Course.js
@@ -0,0 +1,38 @@
+// const htmlBody = document.querySelector('body');
+
+// const randomClickFunction = function(){
+//     const colors = ['yellow', 'green', 'white', 'silver', 'blue', 'red']
+
+//     const randomIndex = Math.floor(Math.random() * colors.length);
+
+//     const randomColor = colors[randomIndex];
+
+//     htmlBody.style.backgroundColor = randomColor;
+
+//     console.log('The user clicked and set the color to ' + randomColor);
+
+// }
+
+// randomClickFunction()
+
+// htmlBody.onclick = randomClickFunction; 
+
+
+// const myFirstVariable = 'Writing code gives you super powers';
+
+// const mySecondVariable = 890;
+
+// const myThirdVariable = { firstProperty: 'So help me LORD Almighty'};
+
+
+const variable1 = 10;
+const variable2 = variable1;
+const variable3 = 'Zach';
+const variable4 = {
+    variableType: 'object',
+    variableValue: 'some value',
+};
+const variable5 = (function(){
+    return 'Hello, my name is ';
+})();
+const variable6 = variable5 + variable3;
